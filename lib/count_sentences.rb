5192ecae9d6e35bf7d6093924bfb0edddd20219e
Tyrone <tyrone.cloma0219@gmail.com>
2022-08-11 10:15:30 -0400

    solution except test

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.split('').last == '.'
   end
 
   def question?
-
+    self.split('').last == '?'
   end
 
   def exclamation?
-
+    self.split('').last == '!'
   end
 
   def count_sentences
-
+    self.split(/\W\s/).count
   end
 end
+
+"hello. hi. what? where!".count_sentences
